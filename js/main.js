748a66f51337792b126ff10157d89ea12631b9e3
charles-tech <charles.fernando.miranda@gmail.com>
2023-03-08 13:36:17 -0300

    Finalizando-Local-Storage

--- js/main.js
+++ js/main.js
@@ -2,49 +2,64 @@ const form = document.getElementById("novoItem");
 const lista = document.getElementById("lista");
 const itens = JSON.parse(localStorage.getItem("itens")) || [];
 
+itens.forEach((elemento) => {
+  console.log(
+    elemento.valorRenda,
+    elemento.valorMercado,
+    elemento.valorLazer,
+    elemento.valorAluguel,
+    elemento.valorInvestidomento,
+    elemento.valorMes
+  );
+});
+exibir();
 
-itens.forEach(elemento => {
-    console.log(elemento.valorRenda, elemento.valorMercado, elemento.valorLazer, elemento.valorAluguel, elemento.valorInvestidomento,elemento.valorTeste)
+form.addEventListener("submit", (evento) => {
+  evento.preventDefault();
+
+  const valorRenda = evento.target.elements["valorRenda"];
+  const valorMercado = evento.target.elements["valorMercado"];
+  const valorLazer = evento.target.elements["valorLazer"];
+  const valorAluguel = evento.target.elements["valorAluguel"];
+  const valorInvestidomento = evento.target.elements["valorInvestidomento"];
+  const valorMes = evento.target.elements["valorMes"];
+
+  const itemAtual = {
+    valorRenda: valorRenda.value,
+    valorMercado: valorMercado.value,
+    valorLazer: valorLazer.value,
+    valorAluguel: valorAluguel.value,
+    valorInvestidomento: valorInvestidomento.value,
+    valorMes: valorMes.value,
+  };
+
+  itens.push(itemAtual);
+  localStorage.setItem("itens", JSON.stringify(itens));
+
+  valorRenda.value = "";
+  valorMercado.value = "";
+  valorLazer.value = "";
+  valorAluguel.value = "";
+  valorInvestidomento.value = "";
+  valorMes.value = "";
 });
 
+function exibir() {
+  const itensCarregado = JSON.parse(localStorage.getItem("itens"));
 
-form.addEventListener("submit",(evento)=>{
-    evento.preventDefault();
+  const valorRenda = document.getElementById("resultadoRenda");
+  const valorMercado = document.getElementById("resultadoMercado");
+  const valorLazer = document.getElementById("resultadoLazer");
+  const valorAluguel = document.getElementById("resultadoAluguel");
+  const valorInvestidomento = document.getElementById("resultadoInvestimento");
+  const valorMes = document.getElementById("resultadoMes");
 
-    const valorRenda = evento.target.elements['valorRenda'];
-    const valorMercado = evento.target.elements['valorMercado'];
-    const valorLazer = evento.target.elements['valorLazer'];
-    const valorAluguel = evento.target.elements['valorAluguel'];
-    const valorInvestidomento = evento.target.elements['valorInvestidomento'];
-
-    const teste = parseInt(valorRenda) *2;
-
-    const itemAtual ={
-        "valorRenda": valorRenda.value,
-        "valorMercado": valorMercado.value,
-        "valorLazer": valorLazer.value,
-        "valorAluguel": valorAluguel.value,
-        "valorInvestidomento": valorInvestidomento.value,
-        "valorTeste": teste.value
-
-    }
-    
-
-
-    itens.push(itemAtual)
-    localStorage.setItem("itens", JSON.stringify(itens))
-
-
-
-    valorRenda.value = ""
-    valorMercado.value = ""
-    valorLazer.value=""
-    valorAluguel.value=""
-    valorInvestidomento.value=""
-
-})
-
-
-
-
-
+  itensCarregado.map((elemento) => {
+    valorRenda.innerHTML += `<p>R$: ${elemento.valorRenda}</p>`;
+    valorMercado.innerHTML += `<p>R$: ${elemento.valorMercado}</p>`;
+    valorLazer.innerHTML += `<p>R$: ${elemento.valorLazer}</p>`;
+    valorAluguel.innerHTML += `<p>R$: ${elemento.valorAluguel}</p>`;
+    valorInvestidomento.innerHTML += `<p>R$: ${elemento.valorInvestidomento}</p>`;
+    valorMes.innerHTML += `<p>Mês: ${elemento.valorMes}</p>`;
+  });
+}
