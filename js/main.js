bff77db4dd63605ff69004a4667dfd10d8e92899
charles-tech <charles.fernando.miranda@gmail.com>
2023-03-09 13:02:40 -0300

    teste

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ form.addEventListener("submit", (evento) => {
   const valorMes = evento.target.elements["valorMes"];
 
   const itemAtual = {
-    valorRenda: { nome: nomeRenda, renda: valorRenda.value },
+    valorRenda: valorRenda.value,
     valorMercado: valorMercado.value,
     valorLazer: valorLazer.value,
     valorAluguel: valorAluguel.value,
